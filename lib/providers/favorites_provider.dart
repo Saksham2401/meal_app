7d03010505d59605de4c57bb4f09c77f6acb6d65
Saksham Verma <93206567+Saksham2401@users.noreply.github.com>
2024-06-14 15:52:52 +0530

    Add files via upload

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final mealisFavourite = state.contains(meal);
+    if (mealisFavourite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
